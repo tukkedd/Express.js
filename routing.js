eb71e0949d65838a3573fef8fce595a206638a9a
tukkedd <jeamki2501@gmail.com>
2024-09-13 18:57:16 -0400

    endpoints get & post with nodejs

--- routing.js
+++ routing.js
@@ -20,12 +20,31 @@ const processRequest = (req, res) => {
             
           case 'POST':
             switch (url) {
-                case './pokemon': {
-                    const body = ''
+                case '/pokemon': {
+                    let body = ''
+                    
+
+                    // escuchar el evento data
+                    req.on('data', chunk => {
+                        body += chunk.toString()
+                    })
+
+                    req.on('end', () => {
+                        const data = JSON.parse(body)
+                        //llamar a una base de datos para guardar la info
+                        res.writeHead(201, {'Content-Type': 'application/json; charset=utf-8'})
+                        res.end(JSON.stringify(data))
+                    })
+
                     break
                 }
-            }
+
+            default: 
+                res.statusCode = 404
+                res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+                return res.end('<h1>404 Not Found</h1>') 
     }   
+  }
 }
 
 const server = http.createServer(processRequest) 
